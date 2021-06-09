fb14081877db190c587477fc0b3078c34b7f5713
ron <musick@wood>
2021-06-09 19:00:29 -0400

    Needed to add fine-grained locking to CEPEQs table.  Dynamo runs requests with several threads.  Saw an interleaving of label and unlabel which lead to a peq being inactive incorrectly.  Now, three-phase locking in peq table rows to avoid interleaving.

--- webServer/tests/testPopulate.js
+++ webServer/tests/testPopulate.js
@@ -492,7 +492,9 @@ async function testIncrementalResolve( authData, ghLinks, td ) {
 
     // Close & accrue
     await tu.closeIssue( authData, td, issPendDat );
+
     await tu.closeIssue( authData, td, issAccrDat );
+    await utils.sleep( 1500 );  // XXX should NOT need this.
     await tu.moveCard( authData, cardAccr.id, accrLoc.colId );
 
     await utils.sleep( 2000 );	
