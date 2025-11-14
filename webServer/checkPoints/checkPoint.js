9ce7e7418ea11b106de7568602e92ef3253e086d
Ron <rmusick@codeequity.net>
2025-11-13 19:09:03 -0500

    Checkpointing is now working.  Using a checkPointHandler in ceRouter, it first checks to see if ceServer is quiescent. If not, fail and report offending jobs.  Then, it requests ceServer block processing the jobQ (jobs will accumulate), and begins working.  Between each checkpointed ceProject, check if ceServer is still quiescent.  If not, fail and report.  If all CEPs are checkpointed, request ceServer to reopen processing of jobQ. CEServer marks the initial blockage, and once the jobQ has at least 1 job on it, if the overall block time exceeds roughly a minute, it will self-unblock. This failsafe is mainly to prevent issues where checkpointing fails in the middle without sending the unblocking request.  The timeout should significantly exceed the possible time required to checkpoint one (the largest) CEP.  Now, set this up as a nightly cron (cron requires a running server).

--- webServer/checkPoints/checkPoint.js
+++ webServer/checkPoints/checkPoint.js
@@ -1,3 +1,4 @@
+import assert from 'assert';
 
 import * as utils from '../utils/ceUtils.js';
 import * as tu    from '../tests/ceTestUtils.js';
@@ -7,36 +8,62 @@ import fs from 'fs';
 import {execSync} from 'child_process';
 import { mkdir } from 'node:fs/promises';
 
+
+async function getJQSize() {
+    let postData = {"Endpoint": "checkPoint", "Request": "getJobQCount" };
+    let count = await utils.postCE( "checkPointHandler", JSON.stringify( postData ));
+    return count;
+}
+
+async function closeJQ() {
+    let postData = {"Endpoint": "checkPoint", "Request": "closeJobQ" };
+    let retVal = await utils.postCE( "checkPointHandler", JSON.stringify( postData ));
+    return retVal;
+}
+
+async function openJQ() {
+    let postData = {"Endpoint": "checkPoint", "Request": "openJobQ" };
+    let retVal = await utils.postCE( "checkPointHandler", JSON.stringify( postData ));
+    return retVal;
+}
+
+async function summarizeJQ() {
+    let postData = {"Endpoint": "checkPoint", "Request": "summarizeJQ" };
+    let retVal = await utils.postCE( "checkPointHandler", JSON.stringify( postData ));
+    return retVal;
+}
+
+
+
 // time stamp is on file.  just want limited queue.  Easiest is by hour.
 // cmd = "aws dynamodb scan --table-name CEPEQs | jq '{"CEPEQA": [.Items[] | {PutRequest: {Item: .}}]}' > testData/testDataCEPEQS.json"
 // cmd = "aws dynamodb scan --table-name CEHostUser --projection-expression "HostPlatform,HostUserName" | jq '{"CEHostUser": [.Items[] | {PutRequest: {Item: .}}]}' > ./testDataCEHostUser.json"
 // cmd = "aws dynamodb scan --table-name CEPEQs --filter-expression "CEProjectId = :value" --expression-attribute-values '{":value": {"S": "CE_FlutTest_ks8asdlg42"}}'"
-async function execAWS_CLI( table, cep ) {
+async function execAWS_CLI( table, cep, fexp, fval, fproj ) {
+
     let success = true;
     const d = new Date();
     let stamp = d.getDate().toString();
     console.log( "Saving AWS Dynamo table:", table );
 
     let dirName = "checkPoints/data/" + cep;
-    await mkdir("./" + dirName, { recursive: true });
     
     let fname = dirName + "/dynamo" + table + "_" + stamp + ".json";
     let lname = dirName + "/dynamo" + table +"_latest"    + ".json";
 	
-    // let cmd = "aws dynamodb scan --table-name " + table + " | jq ";
     let cmd   = "aws dynamodb scan --table-name " + table;
-    cmd = cmd + " --filter-expression \"CEProjectId = :value\" --expression-attribute-values ";
-    cmd = cmd + "'{\":value\": {\"S\": \"" + cep + "\"}}'";
-
-    // Special case to eliminate PATs from hostUser table.  Not a secure fix, just a sloppy joe fix.
-    // XXX Need different filtering.
-    if( table == "CEHostUser" ) {
-	cmd = cmd + " --projection-expression \"HostUserId,CEProjectIds,CEUserId,FutureCEProjects,HostPlatform,HostUserName\" ";
+    if( fexp != "" )
+    {
+	assert( fval != "" );
+	cmd = cmd + " --filter-expression " + fexp + " --expression-attribute-values " + fval;
     }
+
+    if( fproj != "" ) { cmd = cmd + " --projection-expression " + fproj; }
+
     cmd = cmd + " | jq ";
     cmd    += "'{\"" + table + "\": [.Items[] | {PutRequest: {Item: .}}]}' > ";
 
-    console.log( cmd );
+    // console.log( cmd );
     
     execSync( cmd + fname, { encoding: 'utf-8' });
     execSync( cmd + lname, { encoding: 'utf-8' });
@@ -51,70 +78,127 @@ async function execAWS_CLI( table, cep ) {
     return success;
 }
 
+async function writeTables( cev, cep ) {
+
+    console.log( "Working on", cev, cep );
+    let fexp  = "\"CEProjectId = :value\"";
+    let fval  = "'{\":value\": {\"S\": \"" + cep + "\"}}'";
+    let fproj = "";
+	
+    let success = true;
+    let jsize   = 0;
+
+    success = await execAWS_CLI( "CEPEQs", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save PEQ Table" ); }
+    jsize = await getJQSize();
+
+    success = await execAWS_CLI( "CEPEQActions", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save PAct Table" ); }
+    jsize = await getJQSize();
+    
+    success = await execAWS_CLI( "CEPEQRaw", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save raw PAct Table" ); }
+    jsize = await getJQSize();
+    
+    success = await execAWS_CLI( "CEProjects", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Projects Table" ); }
+    jsize = await getJQSize();
+    
+    success = await execAWS_CLI( "CELinkage", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Linkage Table" ); }
+    jsize = await getJQSize();
+    
+    fexp  = "\"PEQSummaryId = :value\"";
+    success = await execAWS_CLI( "CEPEQSummary", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Peq Summary Table" ); }
+    jsize = await getJQSize();
+    
+    fexp  = "\"CEProfileId = :value\"";
+    success = await execAWS_CLI( "CEProfileImage", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Profile Image Table" ); }
+    jsize = await getJQSize();
+    
+    // May have PATS.  Don't save this here
+    fexp  = "\"contains( \"CEProjectIds\", :value )\"";
+    fval  = "'{\":value\": {\"S\": \"" + cep + "\"}}'";
+    fproj = " \"HostUserId,CEProjectIds,CEUserId,FutureCEProjects,HostPlatform,HostUserName\" ";
+    success = await execAWS_CLI( "CEHostUser", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save HostUser Table" ); }
+    jsize = await getJQSize();
+    
+    fexp  = "\"CEVentureId = :value\"";
+    fval  = "'{\":value\": {\"S\": \"" + cev + "\"}}'";
+    fproj = "";
+    success = await execAWS_CLI( "CEVentures", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save CEVenture Table" ); }
+    jsize = await getJQSize();
+    
+    fexp  = "\"EquityPlanId = :value\"";
+    success = await execAWS_CLI( "CEEquityPlan", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Equity Plan Table" ); }
+    jsize = await getJQSize();
+    
+    fexp = "";
+    fval = "";
+    // All public info
+    success = await execAWS_CLI( "CEPeople", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save People Table" ); }
+    jsize = await getJQSize();
+    
+    /*
+    // uninstantiated as of yet
+    success = await execAWS_CLI( "CEAgreements", cep, fexp, fval, fproj );
+    if( !success ) { console.log( "Failed save Agreements Table" ); }
+    */
+
+    return jsize;    
+}
+
 
 // NOTE: Will need to be set up to run createCE.py (i.e. aws cli, etc) in order to run this.
 async function runCP( ) {
 
-    let testStatus = [ 0, 0, []];
-    let success = false;
 
-    // Don't read CEProjects table to get this.  Do not want to checkpoint test CEPs
-    let ceProjects = ["CE_FlutTest_ks8asdlg42", "CE_ServTest_usda23k425", "CE_AltTest_hakeld80a2", "GarlicBeer_38fl0hlsjs", "BookShare_kd8fb.fl9s"]
-
-    success = await execAWS_CLI( "CEHostUser", ceProjects[0] );
-    testStatus = tu.checkEq( success, true, testStatus, "save PEQ Table" );
-
-    return testStatus;
-
-    // XXX
-    // Create dirs once, array of promise.
-    // Then, don't await here.
-
-    // XXX pass filter expression in.  HostUser needs CONTAINS
-    
-    for( const cep of ceProjects ) {
-	
-	success = await execAWS_CLI( "CEPEQs", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save PEQ Table" );
-	
-	success = await execAWS_CLI( "CEPEQActions", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save PAct Table" );
-	
-	success = await execAWS_CLI( "CEPEQSummary", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Peq Summary Table" );
-	
-	success = await execAWS_CLI( "CEPEQRaw", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save raw PAct Table" );
-	
-	success = await execAWS_CLI( "CEProjects", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Projects Table" );
-	
-	success = await execAWS_CLI( "CEPeople", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save People Table" );
-	
-	success = await execAWS_CLI( "CEAgreements", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Agreements Table" );
-	
-	success = await execAWS_CLI( "CELinkage", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Linkage Table" );
-	
-	// XXX Has PATS.  Don't save this here, typically.
-	success = await execAWS_CLI( "CEHostUser", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save HostUser Table" );
-	
-	success = await execAWS_CLI( "CEProfileImage", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Profile Image Table" );
-	
-	success = await execAWS_CLI( "CEEquityPlan", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save Equity Plan Table" );
-	
-	success = await execAWS_CLI( "CEVentures", cep );
-	testStatus = tu.checkEq( success, true, testStatus, "save CEVenture Table" );
+    // Ask ceServer if it is quiescent, if not, checkpointing fails
+    let jqSize = await getJQSize();
+    if( jqSize > 0 ) {
+	console.log( "Error.  Checkpointing failed.  CEServer has pending jobs." );
+	let jqReport = await summarizeJQ();
+	console.log( jqReport );
+	return false;
     }
 
-    return testStatus
+    // otherwise, block the queue
+    await closeJQ();
+
+    // Don't read CEProjects table to get this.  Do not want to checkpoint test CEPs
+    let ceProjects = ["CE_FlutTest_ks8asdlg42", "CE_ServTest_usda23k425", "CE_AltTest_hakeld80a2", "GarlicBeer_38fl0hlsjs", "BookShare_kd8fb.fl9s" ];
+    let ceVentures = ["CE_TEST_Flut_abcde12345", "CE_TEST_Serv_abcde12345", "CE_TEST_Alt_abcde12345", "Connie_Create_4kd8gmc2jf", "BookShare_uvsi38fkg9" ];
+    assert( ceProjects.length == ceVentures.length );
+    
+    // Create dirs once, array of promise.
+    let promises = [];
+    for( const cep of ceProjects ) {
+	let dirName = "checkPoints/data/" + cep;
+	promises.push( mkdir("./" + dirName, { recursive: true }) );
+    }
+    await Promise.all( promises )
+
+    for( let i = 0; i < ceProjects.length; i++ ) {
+	let jsize = await writeTables( ceVentures[i], ceProjects[i] );
+	if( jsize > 0 ) {
+	    console.log( "Error.  Checkpointing did not complete.  CEServer has pending jobs." );
+	    let jqReport = await summarizeJQ();
+	    console.log( jqReport );
+	    break;
+	}
+    }
+
+    // Let ceServer get back to normal
+    await openJQ();
+    return true;
 }
 
 // Switch to runCP rather than exports if npm run checkPoint
-runCP();
+await runCP();
 //export default runCP;
